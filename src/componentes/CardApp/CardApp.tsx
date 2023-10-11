b44c62ef84d7b09782ec41fe17994ca1e1643367
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-10-11 02:03:55 -0300

    Modificado interface de CardApp com buttons proprios

--- src/componentes/CardApp/CardApp.tsx
+++ src/componentes/CardApp/CardApp.tsx
@@ -13,17 +13,17 @@ export const CardApp = ({ app }: CardProps) => {
     const appList = useAppListContext()
 
     const mudaEstado = () => {
-        if (app.estado === "circle"){
+        if (app.estado === "circle") {
             if (appList.appList.length < 5) {
                 app.estado = "check-circle"
                 appList.addToAppList(app)
-                
+
             }
-            else{
+            else {
                 alert("Você já selecionou 5 apps. Por favor, remova algum deles para adicionar um outro")
             }
         }
-        else{
+        else {
             app.estado = "circle"
             appList.removeToAppList(app.id_jogo_steam)
         }
@@ -33,7 +33,25 @@ export const CardApp = ({ app }: CardProps) => {
         <div className={styles.cardContainer}>
             <img className={styles.image} src={app.imagem} />
             <p>{app.nome}</p>
-            <Button style={styles.btnSelect} onClick={() => mudaEstado()} text={app.estado === "check-circle"? "x" : "+"}/>
+            <div
+            className={styles.boxButtons}>
+                <a 
+                    href={`https://store.steampowered.com/app/${app.id_jogo_steam}`}
+                    target="blank"
+                    className={styles.btnSteam}
+                >
+                    Steam
+                </a>
+
+                <button
+                    className={styles.btnSelect}
+                    onClick={() => mudaEstado()}
+                >
+                    {app.estado === "check-circle" ? "Remover" : "Adicionar"}
+                </button>
+
+            </div>
+            {/* <Button style={styles.btnSelect} onClick={() => mudaEstado()} text={app.estado === "check-circle"? "x" : "+"}/> */}
         </div>
     )
 }
