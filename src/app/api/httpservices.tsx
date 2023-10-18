18d0eaf914f6aabf6e8b3a654cdf63c2da72dcb1
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-10-18 20:21:20 -0300

    Movido requisição de cadastro para httpservices

--- src/app/api/httpservices.tsx
+++ src/app/api/httpservices.tsx
@@ -1,16 +1,13 @@
+import { UsuarioProps } from "@/componentes/types";
 import axios from "axios";
 
 const regex = /[^0-9a-zA-Z() " : , { } @ . / -]/gi
 
-const enderecoBackend = "http://164.152.38.61"
+// const enderecoBackend = "http://164.152.38.61"
+const enderecoBackend = "http://localhost:3005"
 
-interface usuarioProps {
-  nome: string;
-  email: string;
-  senha: string;
-}
 
-export async function pesquisaApps(app: string, offset: number) {
+export function pesquisaApps(app: string, offset: number) {
   return axios.get(`https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/pesquisa/resultados/${app}?limit=10000&offset=` + offset)
 }
 
@@ -18,18 +15,11 @@ export function montaPc(requisitos: any) {
   return axios.post(`${enderecoBackend}/montaPc`, { requisitos: requisitos })
 }
 
-export async function cadastraUsuario(usuario: usuarioProps) {
-  return await fetch("http://164.152.38.61/usuario/cadastro", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    body: JSON.stringify({ usuario: usuario }),
-  }
-  )
+export function cadastraUsuario(usuario: UsuarioProps){
+  return axios.post(`${enderecoBackend}/usuario/cadastro`, {usuario:usuario})
 }
 
-export function autenticaUsuario(usuario: usuarioProps) {
+export function autenticaUsuario(usuario: UsuarioProps) {
   return axios.post(`${enderecoBackend}/usuario/autentica`, { usuario: usuario })
 }
 
--- src/componentes/Forms/FormCadastro.tsx
+++ src/componentes/Forms/FormCadastro.tsx
@@ -5,12 +5,13 @@ import { Input } from "../Input/Input";
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { useRouter } from 'next/navigation';
 import { useForm } from "react-hook-form"
-import { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios, { AxiosResponse } from "axios";
 import Router from "next/router";
 import { Button } from "../Button/Button";
+import { AxiosResponse } from "axios";
+import { cadastraUsuario } from "@/app/api/httpservices";
+import { UsuarioProps } from "../types";
 
 const schema = z.object({
     nome: z.string().nonempty("Insira um nome de usuário").min(3, "Insira um nome com no mínimo 3 caracteres"),
@@ -50,22 +51,15 @@ const FormCadastro = () => {
     })
 
     const handleForm = async(data: FormProps) => {
-        const user: object = {
+        const user: UsuarioProps = {
             nome: data.nome,
             email: data.email,
             senha: data.senha
         }
         
-        const resposta: Response = await fetch("http://164.152.38.61/usuario/cadastro", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json;charset=UTF-8",
-            },
-            body: JSON.stringify({ usuario: user }),
-          }
-        )
+        const response: AxiosResponse = await cadastraUsuario(user)
         
-        if (resposta.status === 201) {
+        if (response.status === 201) {
             console.log("cadastrado");
         }
     }
