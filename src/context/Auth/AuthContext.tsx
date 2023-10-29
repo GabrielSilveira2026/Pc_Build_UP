dd002950cff2afb3f1a0216cbe219c90b5dd6cba
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-10-29 08:55:45 -0300

    Ajustes na semantica

--- src/context/Auth/AuthContext.tsx
+++ src/context/Auth/AuthContext.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import {useRouter} from "next/navigation"
-import { setCookie } from "nookies"
+import { setCookie, parseCookies } from "nookies"
 
 import { autenticaUsuario } from '@/app/api/httpservices'
 import { UserProps } from '@/componentes/types'
@@ -18,16 +18,32 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter()
     const [user, setUser] = useState<UserProps | null>(null)
 
+    //recuperar informações do usuario atualizadas
+
+    // useEffect(()=>{
+    //     const { 'PcBuildToken':token } = parseCookies()
+
+    //     if (token) {
+            
+    //     }
+    // },[])
+
     const isAuthenticated = !!user
 
     async function sigiIn({ email, senha }: UserProps) {
         const response = await autenticaUsuario({ email, senha })
         
-        const { token, user } = response.data
+        const { token, 'usuario': user } = response.data
         
         setCookie(undefined, "PcBuildToken", token, {
             maxAge: 60 * 60 * 1//1 hora
         })
+        console.log(user);
+        
+        setCookie(undefined, "UserInfo", JSON.stringify(user), {
+            maxAge: 60 * 60 * 1//1 hora
+        })
+
         setUser(user)
         router.push("/")
     }
